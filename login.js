afba6a43ab589933ce514941f00b410f20cc566a
Pushkar Singh <singhpushkarpss@gmail.com>
2025-07-31 23:25:50 +0530

    Initial commit: Basic login system using Node.js and text file

---
+++ login.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+const readline = require("readline");
+const crypto = require("crypto");
+const path = require("path");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+console.log("Register User");
+rl.question("Enter Your Name : ", (name) => {
+  rl.question("Set Password : ", (pass) => {
+    const hashPassword = crypto.createHash("sha256").update(pass).digest("hex");
+
+    const filepath = path.join(__dirname, "./UserDetails.txt");
+    fs.writeFileSync(filepath, `${name}:${hashPassword}`);
+
+    rl.question("Enter your name : ", (logname) => {
+      rl.question("Enter your password : ", (logpass) => {
+        const hashpass = crypto
+          .createHash("sha256")
+          .update(logpass)
+          .digest("hex");
+
+        
+        const found = false;
+        const details = fs.readFileSync("./UserDetails.txt", "utf-8");
+        const [username, userpass] = details.split(":");
+       
+
+        if (logname == username && hashpass == userpass) {
+            found=true;
+          return console.log("Login successfully");
+        } else {
+          console.log("invalid");
+        }
+
+        rl.close();
+      });
+    });
+
+    /////////////////////////
+  });
+});
